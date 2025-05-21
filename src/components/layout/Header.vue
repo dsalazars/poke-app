cbad563146e2f725813c272e1fefe3ebfa3f41a3
Duvan Salazar <andres53271@hotmail.com>
2025-05-21 12:23:41 -0500

    Added the following components: Header and Footer

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import LogoPokemon from '@/assets/images/logo.webp';
+</script>
+
+<template>
+  <header class="bg-red-600 py-4 border-b-4 border-yellow-400">
+      <!-- Logo -->
+    <div class="container mx-auto px-4 flex items-center justify-center">
+      <img :src="LogoPokemon" alt="Pokémon Logo" class="w-48 max-h-full"/>
+    </div>
+  </header>
+</template>
