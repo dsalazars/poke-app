9ed84ae9ead4feeca800eb380f77e1ebe86c0a77
Duvan Salazar <andres53271@hotmail.com>
2025-05-22 15:13:30 -0500

    Adde test to endpoints pokeapi

---
+++ src/api/__tests__/pokemonApi.spec.js
@@ -0,0 +1,42 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { pokemonService } from '@/api/pokemonApi';
+import pokeApi from '@/api/index';
+
+// Mock del módulo pokeApi
+vi.mock('@/api/index', () => ({
+  default: {
+    get: vi.fn()
+  }
+}));
+
+describe('pokemonService', () => {
+  beforeEach(() => {
+    vi.clearAllMocks(); // Limpia los mocks
+  });
+
+  describe('getPokemonList', () => {
+    it('debe llamar a pokeApi.get con la ruta "pokemon/"', async () => {
+      const mockResponse = { data: { results: ['bulbasaur', 'charmander'] } };
+      pokeApi.get.mockResolvedValue(mockResponse);
+
+      const result = await pokemonService.getPokemonList();
+
+      expect(pokeApi.get).toHaveBeenCalledWith('pokemon/');
+      expect(result).toEqual(mockResponse);
+    });
+  });
+
+  describe('getPokemonDetails', () => {
+    it('debe llamar a pokeApi.get con la ruta "pokemon/{name}"', async () => {
+      const pokemonName = 'pikachu';
+      const mockResponse = { data: { name: 'pikachu' } };
+      pokeApi.get.mockResolvedValue(mockResponse);
+
+      const result = await pokemonService.getPokemonDetails(pokemonName);
+
+      expect(pokeApi.get).toHaveBeenCalledWith(`pokemon/${pokemonName}`);
+      expect(result).toEqual(mockResponse);
+    });
+  });
+});
+
